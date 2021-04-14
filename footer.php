c768c9176e3f444012c191676f57834b31c2bfb7
rherrera <rherrera@syltec.es>
2021-04-14 11:10:51 +0200

    mod-sede

--- footer.php
+++ footer.php
@@ -6,33 +6,31 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package molinadesegura
+ * @package sedeElectronica
  */
+
 ?>
-	<footer id="colophon">
+
+	<footer id="colophon" class="site-footer">
 		<div class="row">
-			<!--Enlaces parte izquierda-->
-			<div class="col-lg-8 col-md-8 col-sm-12 col-12 site-footer">
-				<div class="col-lg-8 col-md-8 col-sm-7 col-7 site-footer-left-links">
-					<a href="http://172.28.0.120/">Inicio</a>
-					<a href="">Ayuntamiento</a>
-					<a href="">Servicios</a>
-					<a href="http://172.28.0.120/sitio2/">Turismo</a>
-					<a href="">Participación</a>
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-5 col-5 site-footer-right-links">
-					<a href="">Contacto</a>
-					<a href="http://172.28.0.120/site-map/">Mapa del Sitio</a>
-					<a href="http://172.28.0.120/sedeelectronica">Sede Electrónica</a>
-					<a href="">Política de Privacidad</a>
-				</div>
+			<div class="img-pie">
+				<img src="http://172.28.0.120/sedeelectronica/wp-content/uploads/sites/4/2021/04/pie.png" alt="">
 			</div>
-			<div class="footer-molina col-12">
+		</div>
+		<div class="row links-1-pie">
+				<div class="col-12 links-1-pie-left">
+					<a href="">Protección de Datos</a>
+				</div>
+				<div class="col-12 links-1-pie-right">
+					<a href="">Aviso Legal</a>
+				</div>
+				<div class="footer-copy col-12">
 				<p class="text-center">Copyright © 2021 Alpha Syltec Ingeniera SLP. Todos los derechos reservados.</p>
 			</div>
 		</div>
+
 	</footer><!-- #colophon -->
-<!-- #page -->
+</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
