32fbab9078b17493440f82940a5d1b02e6ae543c
rherrera <rherrera@syltec.es>
2021-04-30 11:23:34 +0200

    a

--- turismo/footer.php
+++ turismo/footer.php
@@ -14,10 +14,10 @@
 	<footer id="colophon">
 		<div class="row">
 			<div class="col-lg-8 col-md-8 col-sm-12 col-12 div-mapa-img">
-				<div class="col-lg-7 col-md-7 col-sm-12 col-12">
+				<div class="col-lg-9 col-md-7 col-sm-12 col-12">
 		
 				</div>
-				<div class="col-lg-5 col-md-5 col-sm-12 col-12">
+				<div class="col-lg-3 col-md-5 col-sm-12 col-12">
 					<div class="col-lg-12 col-md-12 col-sm-6 col-12">
 						<a href="http://172.28.0.120/" target="_blank"><img src="http://172.28.0.120/wp-content/uploads/2021/03/logos-molina-footer-blanco.png" alt=""></a>
 					</div>
