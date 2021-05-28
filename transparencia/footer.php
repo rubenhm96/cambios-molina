6eb6209175a5e35afd06c79a03fee7628a6af40a
rherrera <rherrera@syltec.es>
2021-05-28 14:37:14 +0200

    tem

---
+++ transparencia/footer.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package transparencia
+ */
+
+?>
+	<footer id="colophon" class="site-footer text-center text-lg-start bg-light text-muted">
+		<!-- Section: Social media -->
+		<section class="d-flex justify-content-center p-4 border-bottom">
+			
+  		</section>
+  		<!-- Section: Social media -->
+
+		<!-- Section: Links  -->
+		<section class="">
+			<div class="container text-center text-md-start mt-5">
+				<!-- Grid row -->
+				<div class="row mt-3">
+					<!-- Grid column -->
+					<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+						
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold mb-4">
+							Otras Sedes
+						</h6>
+						<p>
+							<a href="http://172.28.0.120/" class="text-reset">Ayuntamiento</a>
+						</p>
+						<p>
+							<a href="http://172.28.0.120/sedeelectronica" class="text-reset">Sede Electronica</a>
+						</p>
+						<p>
+							<a href="http://172.28.0.120/turismo" class="text-reset">Turismo</a>
+						</p>
+						<p>
+							<a href="http://172.28.0.120/juventud" class="text-reset">Juventud</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+						<!-- Links -->
+						<h6 class="text-uppercase fw-bold mb-4">
+							Enlaces de Interés
+						</h6>
+						<p>
+							<a href="#!" class="text-reset">Normativa sobre Transparencia</a>
+						</p>
+						<p>
+							<a href="#!" class="text-reset">Informes sobre Transparencia</a>
+						</p>
+						<p>
+							<a href="http://172.28.0.120/transparencia/mapaweb/" class="text-reset">Mapa Web</a>
+						</p>
+					</div>
+					<!-- Grid column -->
+
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+						<!-- Content -->
+						
+						<p class="row">
+							<span class="col-5">
+							<img src="http://172.28.0.120/juventud/wp-content/uploads/sites/11/2021/05/logos-molina-footer-blanco-2.png" alt="">
+							</span>
+							<span class="col-7">
+							<img src="http://172.28.0.120/juventud/wp-content/uploads/sites/11/2021/05/logo-dereca.png" alt="">
+							</span>
+						</p>
+						<p class="row text-end">
+							<span class="col-5">
+								<img src="http://172.28.0.120/juventud/wp-content/uploads/sites/11/2021/05/logo-feder-ue-footer-2.png" alt="">
+							</span>
+						</p>
+					</div>
+					<!-- Grid column -->
+				</div>
+				<!-- Grid row -->
+			</div>
+		</section>
+		<!-- Section: Links  -->
+
+		<!-- Copyright -->
+		<div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+			© 2021 Copyright
+			<a class="text-reset fw-bold" href="http://172.28.0.120/juventud">Molina de Segura</a>
+		</div>
+		<!-- Copyright -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ transparencia/header.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package transparencia
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'transparencia' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="max-width-content site-header-in">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				?>
+			</div><!-- .site-branding -->
+			<div class="buscador-ajax">
+				<?php echo do_shortcode('[wd_asp id=5]'); ?>
+			</div>
+		</div>
+		<nav id="site-navigation" class="main-navigation">
+			<?php echo do_shortcode('[maxmegamenu location=menu-1]')?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
